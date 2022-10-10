1d43271a9b7f60aad0b29ec91666bcfc7a98787c
Roland Doll <83877587+RDoll-Repo@users.noreply.github.com>
2022-10-10 14:27:41 -0400

    seed series

---
+++ OmniGLM-API/Scripts/Seeds/001.do.seed_sample_categories.sql
@@ -0,0 +1,49 @@
+INSERT INTO
+    series(series_title)
+
+VALUES
+    ('Pikmin'),
+    ('Mana'),
+    ('Assassin''s Creed'),
+    ('Xeno'),
+    ('Final Fantasy');
+
+
+INSERT INTO
+    genres(genre_name)
+
+VALUES
+    ('Visual Novel'),
+    ('Real-time Strategy')
+    ('Simulation'),
+    ('Roguelike'),
+    ('Japanese RPG'),
+    ('Action-Adventure'),
+    ('Rail Shooter');
+
+
+INSERT INTO
+    consoles(console_name)
+
+VALUES
+    ('PC'),
+    ('GameCube'),
+    ('Xbox 360'),
+    ('Playstation 4'),
+    ()
+
+
+INSERT INTO
+    games(title, series_id, genre_id, console_id, release_date, status, length, date_added, date_completed, blocked_by, format, condition, notes)
+
+VALUES
+    ('HuniePop', null, 'genre', 'console', '2015-1-19', 'PLAYING', 10, now(), null, null, 'DIGITAL', 'N/A', 'This game is gross'),
+    ('Pikmin 2', '', ),
+    ('Way of the Samurai 3'),
+    ('Dead Cells'),
+    ('Secret of Mana'),
+    ('Assassin''s Creed'),
+    ('Tin Star'),
+    ('Rogue Galaxy'),
+    ('Xenoblade Chronicles 3'),
+    ('Final Fantasy XV: Episode Duscae'),
