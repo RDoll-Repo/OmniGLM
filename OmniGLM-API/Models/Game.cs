f0d599317e45044183f0276c1d37bb3f536efacd
Roland Doll <83877587+RDoll-Repo@users.noreply.github.com>
2022-11-05 22:18:23 -0400

    enums work, FOR NOW

--- OmniGLM-API/Models/Game.cs
+++ OmniGLM-API/Models/Game.cs
@@ -1,4 +1,5 @@
 using OmniGLM_API.db;
+using System.Text.Json.Serialization;
 
 namespace OmniGLM_API.Models
 {
@@ -10,12 +11,18 @@ namespace OmniGLM_API.Models
         public Genre Genre { get; set; }
         public Console Console { get; set; }
         public DateTime ReleaseDate { get; set; }
+
+        [JsonConverter(typeof(JsonStringEnumConverter))]
         public Status Status { get; set; }
         public int Length { get; set; }
         public DateTime? DateAdded { get; set; }
         public DateTime? DateCompleted { get; set; }
         public Game? BlockedBy { get; set; }
+
+        [JsonConverter(typeof(JsonStringEnumConverter))]
         public Format? Format { get; set; }
+
+        [JsonConverter(typeof(JsonStringEnumConverter))]
         public Condition? Condition { get; set; }
         public string? Notes { get; set; }
     }
