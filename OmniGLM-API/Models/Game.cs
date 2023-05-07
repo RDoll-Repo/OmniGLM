ff7975ac703a4a8123f5e1b4d6382c3626c09077
Roland Doll <83877587+RDoll-Repo@users.noreply.github.com>
2023-05-07 16:51:14 -0400

    basic game implementation

--- OmniGLM-API/Models/Game.cs
+++ OmniGLM-API/Models/Game.cs
@@ -1,10 +1,25 @@
 using OmniGLM_API.db;
-using System.Text.Json.Serialization;
 
 namespace OmniGLM_API.Models
 {
     public class Game : IEntity<Guid>
     {
         public Guid Id { get; set; }
+        public string Title { get; set; }
+
+        // TODO: Enum
+        public string Status { get; set; }
+
+        // TODO: Normalize
+        public string Console { get; set; }
+
+        // TODO: Enum
+        public string Format { get; set; }
+
+        // TODO: Normalize
+        public string Genre { get; set; }
+        public int Length { get; set; }
+        public DateTime DateAdded { get; set; } = DateTime.Now;
+        public DateTime? DataCompleted { get; set; }
     }
 }
