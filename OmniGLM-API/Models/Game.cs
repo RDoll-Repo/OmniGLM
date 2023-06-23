173e88ffc42bd15aeda874a42b533733769e889a
Roland Doll <83877587+RDoll-Repo@users.noreply.github.com>
2023-06-23 12:47:23 -0400

    started addgamepayload

--- OmniGLM-API/Models/Game.cs
+++ OmniGLM-API/Models/Game.cs
@@ -7,15 +7,34 @@ namespace OmniGLM_API.Models
         public Guid Id { get; set; }
         public string Title { get; set; }
         public Status Status { get; set; }
-        public Console Console { get; set; }
-
-        // TODO: Enum
-        public string Format { get; set; }
-
-        // TODO: Normalize
-        public string Genre { get; set; }
+        public Guid ConsoleId { get; set; }
+        public Format Format { get; set; }
+        public ICollection<Genre> Genres { get; set; }
         public int Length { get; set; }
         public DateTime DateAdded { get; set; } = DateTime.Now;
         public DateTime? DateCompleted { get; set; }
+
+        public Game()
+        {
+
+        }
+    }
+
+    public class AddGamePayload
+    {
+        public string Title { get; set; }
+        public Status Status { get; set; }
+        public Guid ConsoleId { get; set; }
+        public Format Format { get; set; }
+        public List<Guid> GenreIds { get; set; }
+        public int Length { get; set; }
+        public DateTime DateAdded { get; set; } = DateTime.Now;
+        public DateTime? DateCompleted { get; set; }
+
+        public bool IsValid =>
+            !String.IsNullOrWhiteSpace(Title)
+                && ConsoleId != Guid.Empty
+                && GenreIds.Count > 0
+                && Length > 0;
     }
 }
