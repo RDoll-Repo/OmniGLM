9f040db1c34ecbd3d0423790c7b1b897cf000f5f
Roland Doll <83877587+RDoll-Repo@users.noreply.github.com>
2024-01-28 02:11:47 -0500

    response looks 100% correct now

--- OmniGLM-API/Models/Game.cs
+++ OmniGLM-API/Models/Game.cs
@@ -1,13 +1,19 @@
+using System.Text.Json.Serialization;
 using OmniGLM_API.db;
 
 namespace OmniGLM_API.Models
 {
     public class Game : IEntity<Guid>
     {
+        // TODO: Move these enum converters to an actual viewmodel
         public Guid Id { get; set; }
         public string Title { get; set; }
+
+        [JsonConverter(typeof(JsonStringEnumConverter))]
         public Status Status { get; set; }
         public virtual Console Console { get; set; }
+
+        [JsonConverter(typeof(JsonStringEnumConverter))]
         public Format Format { get; set; }
         public virtual Genre Genre { get; set; }
         public int Length { get; set; }
