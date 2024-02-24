7939da7466cbd56e645779d54c24fd7b0e269dca
Roland Doll <83877587+RDoll-Repo@users.noreply.github.com>
2024-02-24 12:33:39 -0500

    schema update

--- OmniGLM-API/Models/Console.cs
+++ OmniGLM-API/Models/Console.cs
@@ -6,5 +6,7 @@ namespace OmniGLM_API.Models
     {
         public Guid Id { get; set; }
         public string Title { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
     }
 }
