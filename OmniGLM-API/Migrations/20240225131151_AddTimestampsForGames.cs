00a0bdde1574861f76c9ee8a41e940487f76b299
Roland Doll <83877587+RDoll-Repo@users.noreply.github.com>
2024-02-25 08:13:53 -0500

    migration

---
+++ OmniGLM-API/Migrations/20240225131151_AddTimestampsForGames.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OmniGLM_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTimestampsForGames : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "date_added",
+                table: "games",
+                newName: "created_at");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "updated_at",
+                table: "games",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "updated_at",
+                table: "games");
+
+            migrationBuilder.RenameColumn(
+                name: "created_at",
+                table: "games",
+                newName: "date_added");
+        }
+    }
+}
