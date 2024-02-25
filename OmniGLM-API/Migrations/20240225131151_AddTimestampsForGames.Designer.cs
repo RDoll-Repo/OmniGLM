e87508337a151b9116560470ffe76c60ce4952f5
Roland Doll <83877587+RDoll-Repo@users.noreply.github.com>
2024-02-25 09:03:38 -0500

    Feature/OGLM-12 Add Created/Updated At Timestamps to Games (#16)
    * spec and erd
    
    * migration

--- OmniGLM-API/Migrations/20240225131151_AddTimestampsForGames.Designer.cs
+++ OmniGLM-API/Migrations/20240225131151_AddTimestampsForGames.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using OmniGLM_API.db;
@@ -11,9 +12,11 @@ using OmniGLM_API.db;
 namespace OmniGLM_API.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20240225131151_AddTimestampsForGames")]
+    partial class AddTimestampsForGames
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
