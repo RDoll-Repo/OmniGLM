2eed7369b51bdb46ee22625c40fde60edb428a99
Roland Doll <83877587+RDoll-Repo@users.noreply.github.com>
2024-04-27 01:23:33 -0400

    it really just worked out of the box. okay.

--- OmniGLM-API/Services/LibraryService.cs
+++ OmniGLM-API/Services/LibraryService.cs
@@ -8,6 +8,7 @@ namespace OmniGLM_API.Services
         Task<ApiResponse<GameViewModel>> CreateGameAsync(CreateGamePayload p);
         Task<ApiResponse<SearchGamesMeta, SearchGamesData>> GetLibraryAsync();
         Task<ApiResponse<GameViewModel>> FetchGameAsync(Guid id);
+        Task<ApiResponse<GameViewModel>> UpdateGameAsync(Guid id, UpdateGamePayload p);
         Task DeleteGameAsync(Guid id);
     }
 
@@ -59,6 +60,25 @@ namespace OmniGLM_API.Services
             {
                 Data = new GameViewModel(result)
             };
+        }
+
+        public async Task<ApiResponse<GameViewModel>> UpdateGameAsync(Guid id, UpdateGamePayload p)
+        {
+            var game = await _repo.FetchAsync(id) ?? throw new Exception("Not found");
+
+            game.UpdateGame(p);
+
+            var result = await _repo.UpdateAsync(game);
+
+            // TODO: Find a better way to do this
+            var entity = await _repo.FetchAsync(result.Id);
+
+            var response = new ApiResponse<GameViewModel>
+            {
+                Data = new GameViewModel(entity)
+            };
+
+            return response;
         }
 
         public async Task DeleteGameAsync(Guid id)
