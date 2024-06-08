94f237cd3fdb49387e300acf51c361f4af5342c8
Roland Doll <83877587+RDoll-Repo@users.noreply.github.com>
2024-06-08 05:33:26 -0400

    audit documentation and migrations

--- OmniGLM-API/Migrations/demo.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO consoles VALUES
-('482b78b3-a7cd-43ef-ab56-3b5f1befb737', 'PS4');
-
-INSERT INTO genres VALUES
-('a76ebb38-0a3f-454b-b18c-f22f9c8d787a', 'JRPG');
-
-INSERT INTO games VALUES
-('d703e204-c73c-48ab-a0c1-e44ad056f82d', 'FF16', 1, 1, 30, NOW(), NOW(), '482b78b3-a7cd-43ef-ab56-3b5f1befb737', 'a76ebb38-0a3f-454b-b18c-f22f9c8d787a')
